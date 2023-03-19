ce0ca175522a20925017227fc5aa9cdeb9459a62
Oluwatobi <oluwatobiogungbangbe@gmail.com>
2023-03-19 18:28:42 +0000

    Adding Apple Calculator scrip

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let display=document.getElementById("screen");
 function allclear(){
-    display.value="";
+    display.value = "";
+}
+function clear(){
+    display.value = display.value.slice(0, -1);
 }
 function show(n){
-    display.value+=n;
+    display.value += n;
 }
 function calc(){
     display.value = eval(display.value);
